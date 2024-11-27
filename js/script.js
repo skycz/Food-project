eb0453aef74c42c877a0f586007d681169e34f20
Tamerlan <example@gmail.com>
2024-11-27 16:59:55 +0300

    Forms PHP

--- js/script.js
+++ js/script.js
@@ -231,7 +231,48 @@ window.addEventListener('DOMContentLoaded', () => {
         21,
         '.menu .container',
         'menu__item'
-    ).render(); // 
-    Добавляем еще карточки
+    ).render(); // Добавляем еще карточки
+    
+    // Forms 
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'multipart/form-data');
+            const formData = new FormData(form);
+
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
 
