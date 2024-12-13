1ae015b4a20053930b9b5a1d3d2386b8a8c7c55a
nothing here <roninfortnite71@gmail.com>
2024-12-13 21:27:45 +0300

    Update services.js

--- js/services/services.js
+++ js/services/services.js
@@ -1,26 +1,32 @@
-// async, await (POST)
+// Функция для отправки POST-запроса с использованием async/await
 const postData = async (url, data) => {
+    // Используем fetch API для отправки POST-запроса на указанный URL
     const res = await fetch(url, {
-        method: 'POST',
+        method: 'POST',  // Метод HTTP-запроса (POST)
         headers: {
-            'Content-type': 'application/json'
+            'Content-type': 'application/json'  // Указываем тип содержимого, что это JSON
         },
-        body: data
+        body: data  // Отправляем данные в теле запроса
     });
 
+    // Ждем ответа от сервера и возвращаем результат в формате JSON
     return await res.json();
 };
 
-// async, await (GET)
+// Функция для отправки GET-запроса с использованием async/await
 const getResource = async (url) => {
+    // Используем fetch API для отправки GET-запроса на указанный URL
     const res = await fetch(url);
 
+    // Проверяем, если ответ не успешный, выбрасываем ошибку
     if (!res.ok) {
         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
+    // Ждем ответа от сервера и возвращаем результат в формате JSON
     return await res.json();
 }
 
+// Экспортируем обе функции для использования в других частях приложения
 export {postData};
 export {getResource};
